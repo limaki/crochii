165fd53df51687712faa04df86905d15b1c24ffb
myusername <UlisesOviedou170@gmail.com>
2025-08-16 03:46:55 -0300

    Actualización del backend

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,13 +1,13 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000'
+// export const environment = {
+//     production: false,
+//     apiUrl: 'http://localhost:3000'
+//   };
+
+
+//  produccion real : 
+
+  export const environment = {
+    production: true,
+    apiUrl: 'https://backend69-1.onrender.com' 
   };
-
-
- // produccion real : 
-
-  // export const environment = {
-  //   production: true,
-  //   apiUrl: 'https://backend69-1.onrender.com' 
-  // };
 
